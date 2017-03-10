1119ba3418a53c49c4a46833ac3f6b6db954aaf1
TommyO <tworeilly@gmail.com>
2017-03-10 00:52:03 -0500

    Initial attempts to test android direct access.

--- src/android/CBLite.java
+++ src/android/CBLite.java
@@ -1,4 +1,4 @@
-package com.couchbase.cblite.phonegap;
+package com.ussieapp.cblite.phonegap;
 
 import android.content.Context;
 
@@ -9,26 +9,30 @@ import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
 
 import com.couchbase.lite.android.AndroidContext;
+
 import com.couchbase.lite.Database;
+import com.couchbase.lite.DatabaseOptions;
 import com.couchbase.lite.Manager;
+/*
 import com.couchbase.lite.listener.LiteListener;
 import com.couchbase.lite.listener.LiteServlet;
 import com.couchbase.lite.listener.Credentials;
-import com.couchbase.lite.router.URLStreamHandlerFactory;
+*/
 import com.couchbase.lite.View;
+
 import com.couchbase.lite.javascript.JavaScriptReplicationFilterCompiler;
 import com.couchbase.lite.javascript.JavaScriptViewCompiler;
 import com.couchbase.lite.util.Log;
 
 import java.io.IOException;
-import java.io.File;
+// import java.io.File;
 
 public class CBLite extends CordovaPlugin {
 
-	private static final int DEFAULT_LISTEN_PORT = 5984;
+	private static Manager manager;
+	private Map<String, String> dbs;
+
 	private boolean initFailed = false;
-	private int listenPort;
-    private Credentials allowedCredentials;
 
 	/**
 	 * Constructor.
@@ -38,98 +42,54 @@ public class CBLite extends CordovaPlugin {
 		System.out.println("CBLite() constructor called");
 	}
 
+	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		System.out.println("initialize() called");
 
 		super.initialize(cordova, webView);
-		initCBLite();
 
-	}
+		dbs = new HashMap<String, Database>();
 
-	private void initCBLite() {
 		try {
-
-		    allowedCredentials = new Credentials();
-
-			URLStreamHandlerFactory.registerSelfIgnoreError();
-
 			View.setCompiler(new JavaScriptViewCompiler());
 			Database.setFilterCompiler(new JavaScriptReplicationFilterCompiler());
 
-			Manager server = startCBLite(this.cordova.getActivity());
-
-			listenPort = startCBLListener(DEFAULT_LISTEN_PORT, server, allowedCredentials);
-
-			System.out.println("initCBLite() completed successfully");
-
-
-		} catch (final Exception e) {
-			e.printStackTrace();
-			initFailed = true;
-		}
-
-	}
-
-	@Override
-	public boolean execute(String action, JSONArray args,
-			CallbackContext callback) {
-		if (action.equals("getURL")) {
-			try {
-
-				if (initFailed == true) {
-					callback.error("Failed to initialize couchbase lite.  See console logs");
-					return false;
-				} else {
-					String callbackRespone = String.format(
-							"http://%s:%s@localhost:%d/",
-                            allowedCredentials.getLogin(),
-                            allowedCredentials.getPassword(),
-                            listenPort
-                    );
-
-					callback.success(callbackRespone);
-
-					return true;
-				}
-
-			} catch (final Exception e) {
-				e.printStackTrace();
-				callback.error(e.getMessage());
-			}
-		}
-		return false;
-	}
-
-	protected Manager startCBLite(Context context) {
-		Manager manager;
-		try {
-		        Manager.enableLogging(Log.TAG, Log.VERBOSE);
+			Manager.enableLogging(Log.TAG, Log.VERBOSE);
 			Manager.enableLogging(Log.TAG_SYNC, Log.VERBOSE);
 			Manager.enableLogging(Log.TAG_QUERY, Log.VERBOSE);
 			Manager.enableLogging(Log.TAG_VIEW, Log.VERBOSE);
 			Manager.enableLogging(Log.TAG_CHANGE_TRACKER, Log.VERBOSE);
 			Manager.enableLogging(Log.TAG_BLOB_STORE, Log.VERBOSE);
 			Manager.enableLogging(Log.TAG_DATABASE, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_LISTENER, Log.VERBOSE);
 			Manager.enableLogging(Log.TAG_MULTI_STREAM_WRITER, Log.VERBOSE);
 			Manager.enableLogging(Log.TAG_REMOTE_REQUEST, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_ROUTER, Log.VERBOSE);
-			manager = new Manager(new AndroidContext(context), Manager.DEFAULT_OPTIONS);
-			
-		} catch (IOException e) {
+			manager = new Manager(new AndroidContext(this.cordova.getActivity()), Manager.DEFAULT_OPTIONS);
+		} catch (final Exception e) {
+			e.printStackTrace();
+			initFailed = true;
 			throw new RuntimeException(e);
 		}
-		return manager;
 	}
 
-	private int startCBLListener(int listenPort, Manager manager, Credentials allowedCredentials) {
-
-		LiteListener listener = new LiteListener(manager, listenPort, allowedCredentials);
-		int boundPort = listener.getListenPort();
-		Thread thread = new Thread(listener);
-		thread.start();
-		return boundPort;
-
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException {
+		if (initFailed == true) {
+			callback.error("Failed to initialize couchbase lite.  See console logs");
+			return false;
+		}
+		try {
+			String name = args.getString(0);
+			switch (action) {
+				case "open":
+					return this.open(name, callback);
+				case "close":
+					return this.close(name, callback);
+			}
+		} catch (final Exception e) {
+			e.printStackTrace();
+			callback.error(e.getMessage());
+		}
+		return false;
 	}
 
 	public void onResume(boolean multitasking) {
@@ -139,6 +99,23 @@ public class CBLite extends CordovaPlugin {
 	public void onPause(boolean multitasking) {
 		System.out.println("CBLite.onPause() called");
 	}
+
+	private boolean open(String name, CallbackContext callback) throws JSONException {
+		DatabaseOptions options = new DatabaseOptions();
+		options.setCreate(true);
+
+		Database database = manager.openDatabase(name, options);
+		dbs.put(name, database);
+		callback.success();
+		return true;
+	}
+
+	private boolean close(String name, CallbackContext callback) throws JSONException {
+		dbs.remove(name);
+		callback.success();
+		return true;
+	}
+
 
 
 }
